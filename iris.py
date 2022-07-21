9bacbd9d3fcca8956c2cb2e93b3f21e03496184f
Puteri Balqis <109638972+ptbalqis@users.noreply.github.com>
2022-07-21 15:55:20 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -38,7 +38,7 @@ prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
 st.write(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'])  
-st.write(df)
+st.write(table)
 
 st.subheader('Prediction')
 st.write(prediction)
